a6d12c24c94f0d8a0a8b56bca278f990794c499f
Brad Buchanan <bradley.c.buchanan@gmail.com>
2022-06-09 23:13:11 -0700

    Tests capture transcript

--- test/blessed-paths.js
+++ test/blessed-paths.js
@@ -27,73 +27,8 @@ Debugging needs when an error is found:
   State dump to check hidden information.
  */
 
-const puppeteer = require('puppeteer');
-const path = require('path');
 const test = require('tape');
-
-class Game {
-    async setup() {
-        this.browser = await puppeteer.launch({
-            // headless: false,
-            // devtools: true,
-            // slowMo: 25,
-        });
-        this.page = await this.browser.newPage();
-        await this.page.goto(`file:${path.join(__dirname, '..', 'main.html')}`);
-
-        // Capture initial saveHash for quick restart
-        this.initialSaveHash = await this.page.evaluate(() => window.story.saveHash());
-    }
-
-    async teardown() {
-        await this.browser.close();
-    }
-
-    async restart() {
-        await this.page.evaluate((h) => window.story.restore(h), this.initialSaveHash);
-    }
-
-    /**
-     * Walk a series of links in sequence, with validations.
-     * @param {Array.{string]} steps Text of links to click, in order
-     */
-    async walk(steps) {
-        while (steps.length > 0) {
-            await this.clickLink(steps.shift());
-            await this.assertNoError();
-        }
-    }
-
-    async clickLink(linkText) {
-        const passage = await this.page.$('tw-passage');
-        const links = await passage.$$('a');
-        for (let link of links) {
-          const text = await link.evaluate(l => l.innerText);
-          if (linkText === text) {
-              await link.click();
-              return;
-          }
-        }
-
-        const passageText = await this.page.$eval('tw-passage', p => p.innerText);
-        throw new Error(`Expected a link with text "${linkText}" but none was found in passage:\n\n${passageText}`);
-    }
-
-    async assertText(expectedText) {
-        const passageText = await this.page.$eval('tw-passage', el => el.innerText);
-        if (!passageText.includes(expectedText)) {
-            throw new Error(`Expected text "${expectedText}" but it was not found in passage:\n\n${passageText}`);
-        }
-    }
-
-    async assertNoError() {
-        const passage = await this.page.$('tw-passage');
-        if (!passage) {
-          const errorMessage = await this.page.$eval('tw-story', el => el.innerText);
-          throw new Error(`Twine reported an error: ${errorMessage}`);
-        }
-      }
-}
+const Game = require('./Game');
 
 test('Blessed Paths', async t => {
     const game = new Game();
@@ -130,6 +65,7 @@ test('Blessed Paths', async t => {
             `You attend to one of the thoughts.`,
         ]);
         await game.assertText(`END OF PROTOTYPE`);
+        console.log(game.currentKnot.transcript());
         t.end();
     });
 
@@ -137,8 +73,32 @@ test('Blessed Paths', async t => {
         await game.restart();
         await game.walk([
             'Play as Flora',
-            'You’re in the dining car.'
+            'You’re in the dining car.',
+            'You observe in disgust.',
+            'partnered up with?',
+            'odd.',
+            'What exactly are we hunting?',
+            `What is that necklace you're wearing?`,
+            `Holy`,
+            `How will we find the vampire when we get to Schattental?`,
+            `Castle`,
+            `Do you have a vampire book I could read?`,
+            `Homework`,
+            `Why is your skin so pale?`,
+            `Bloody`,
+            `You push your plate away.`,
+            `Maybe you are more alone than you realized.`,
+            `You walk out of the car without looking back.`,
+            `Someone offers their lighter.`,
+            `He’s friendly enough.`,
+            `You tell him everything.`,
+            `You take a shaky drag on your cigarette.`,
+            `You agree, but not tonight.`,
+            `head back inside.`,
+            `We’ve reached the mountains.`,
+            `You’re not quite ready for sleep.`,
         ]);
+        await game.assertText(`END OF PROTOTYPE`);
         t.end();
     });
 });
